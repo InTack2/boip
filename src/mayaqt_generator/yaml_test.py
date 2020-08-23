5c600545ecd9c1c24485e2c73669ddb0bea08b2a
Tack2 <takumi236@gmail.com>
2020-08-23 20:29:08 +0900

    delete yaml test files.

--- src/mayaqt_generator/yaml_test.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import os
-import pprint
-
-sample_yaml = os.path.join(os.path.dirname(__file__), "sample.yaml")
-
-with open(sample_yaml) as f:
-    yml = yaml.load(f)
-    pprint.pprint(yml)
