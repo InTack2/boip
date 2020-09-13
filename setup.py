feacd6573663d32fa1b54e865289ba54782f93dc
Tack2 <takumi236@gmail.com>
2020-09-13 22:02:16 +0900

    setup.pyの更新

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
-install_requires = [
-    "pyyaml",
-    "inquirer",
-]
 
-
-setup(
-    name='boip',
-    version='0.1',
-    install_requires=install_requires,
-    tests_require="pytest",
-)
+setup()
