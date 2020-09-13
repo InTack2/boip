aaa028008152ed50a01c25ec983a9610a828821e
Tack2 <takumi236@gmail.com>
2020-09-13 22:07:38 +0900

    フォルダー名を変更

--- src/boip/__init__.py
+++ src/boip/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from __future__ import unicode_literals
@@ -6,7 +5,4 @@ from __future__ import absolute_import
 from __future__ import generators
 from __future__ import division
 
-__version__ = '0.1.0'
-
-if __name__ == "__main__":
-    pass
+__version__ = '0.0.1'
