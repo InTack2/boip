99a2c1cf7116dc4a28453d44ac9768446241174d
Tack2 <takumi236@gmail.com>
2020-09-29 00:37:21 +0900

    boip version up to 1.0.3

--- src/boip/__init__.py
+++ src/boip/__init__.py
@@ -4,4 +4,4 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 from __future__ import generators
 from __future__ import division
-__version__ = '0.0.2'
+__version__ = '0.0.3'
